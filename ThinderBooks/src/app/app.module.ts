a48e8201b21e8774ae7626e616bfd4bf5ac099f5
Edionay <edionay@gmail.com>
2017-11-27 04:23:34 -0200

    Inserindo módulos Cartão, Estante e Login.

--- ThinderBooks/src/app/app.module.ts
+++ ThinderBooks/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EstanteComponent } from './estante/estante.component';
+import { CartaoDeLivroComponent } from './cartao-de-livro/cartao-de-livro.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstanteComponent
+    EstanteComponent,
+    CartaoDeLivroComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
