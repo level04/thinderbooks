ce1deca1a9000f6e7158caf9480ddaffbd63d281
Edionay <edionay@gmail.com>
2017-11-27 01:05:05 -0200

    Criação do componente Estante.

--- ThinderBooks/src/app/app.module.ts
+++ ThinderBooks/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EstanteComponent } from './estante/estante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstanteComponent
   ],
   imports: [
     BrowserModule
