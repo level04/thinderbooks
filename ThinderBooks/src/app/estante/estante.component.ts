7b20b351540d8a1f381d5e10002cb05eb81afe34
Edionay <edionay@gmail.com>
2017-11-27 04:22:31 -0200

    Importando livros externos.

--- ThinderBooks/src/app/estante/estante.component.ts
+++ ThinderBooks/src/app/estante/estante.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Livro} from '../livro';
+import {LIVROS} from '../../assets/dados/livro.dados';
 
 @Component({
   selector: 'app-estante',
   templateUrl: './estante.component.html',
   styleUrls: ['./estante.component.css']
 })
+
 export class EstanteComponent implements OnInit {
-
-  constructor() { }
-
+  livros: Livro[];
+  constructor() {
+  }
   ngOnInit() {
+    this.livros = LIVROS;
+    console.log(this.livros);
   }
 
 }
