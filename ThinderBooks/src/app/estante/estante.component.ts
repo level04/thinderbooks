a985c02b7102eb70ff57016abf6e36f45577290b
Edionay <edionay@gmail.com>
2017-11-27 01:04:36 -0200

    Criação do componente Estante.

---
+++ ThinderBooks/src/app/estante/estante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estante',
+  templateUrl: './estante.component.html',
+  styleUrls: ['./estante.component.css']
+})
+export class EstanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
