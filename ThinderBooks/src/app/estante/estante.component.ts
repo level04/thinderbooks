6b1315fea22c9e8b98b42f418308da74a6550944
Edionay <edionay@gmail.com>
2017-11-27 23:25:34 -0200

    Estante global e de usuário.

--- ThinderBooks/src/app/estante/estante.component.ts
+++ ThinderBooks/src/app/estante/estante.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Livro} from '../livro';
 import {LIVROS} from '../../assets/dados/livro.dados';
+import {Usuario} from '../usuario';
+import {LoginComponent} from '../login/login.component';
 
 @Component({
   selector: 'app-estante',
@@ -9,12 +11,48 @@ import {LIVROS} from '../../assets/dados/livro.dados';
 })
 
 export class EstanteComponent implements OnInit {
-  livros: Livro[];
-  constructor() {
+  todosOsLivros: Livro[];
+  livrosDoUsuario: Livro[];
+  resultadosDaBuscaGlobal: Livro[];
+  resultadosDaBuscaDoUsuario: Livro[];
+  usuario: Usuario;
+  constructor(private login: LoginComponent) {
+    this.usuario = login.usuarioAtual;
   }
   ngOnInit() {
-    this.livros = LIVROS;
-    console.log(this.livros);
+    this.livrosDoUsuario = [];
+    this.todosOsLivros = LIVROS;
+    this.resultadosDaBuscaGlobal = this.todosOsLivros;
+    this.usuario.livros.forEach(idDoLivro => {
+      this.livrosDoUsuario.push(this.obterLivroPeloId(idDoLivro));
+    });
+    this.resultadosDaBuscaDoUsuario = this.livrosDoUsuario;
+    console.log(this.todosOsLivros);
+    console.log(this.usuario.livros);
+    console.log(this.livrosDoUsuario);
   }
 
+  filtroGlobal(termo: string) {
+    termo = termo.toLowerCase();
+    this.resultadosDaBuscaGlobal = this.todosOsLivros;
+    this.resultadosDaBuscaGlobal = this.resultadosDaBuscaGlobal.filter(livro => {
+      return livro.titulo.toLowerCase().includes(termo);
+    });
+  }
+
+  filtroDoUsuario(termo: string) {
+    termo = termo.toLowerCase();
+    this.resultadosDaBuscaDoUsuario = this.livrosDoUsuario;
+    this.resultadosDaBuscaDoUsuario = this.resultadosDaBuscaDoUsuario.filter(livro => {
+      return livro.titulo.toLowerCase().includes(termo);
+    });
+  }
+  obterLivroPeloId(id: number): Livro {
+    for (const livro of this.todosOsLivros) {
+      if (id === livro.id) {
+        return livro;
+      }
+    }
+    return null;
+  }
 }
