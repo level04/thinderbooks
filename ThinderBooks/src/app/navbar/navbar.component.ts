66c50503d271b40ea594fdbf1714a9f2a7b24167
Edionay <edionay@gmail.com>
2017-11-27 23:20:59 -0200

    Incluindo dropdown de usuário.

--- ThinderBooks/src/app/navbar/navbar.component.ts
+++ ThinderBooks/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from '../usuario';
+import { LoginComponent} from '../login/login.component';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+  constructor(private usuarioLogado: LoginComponent) {
+    this.usuario = usuarioLogado.usuarioAtual;
+  }
 
   ngOnInit() {
   }
